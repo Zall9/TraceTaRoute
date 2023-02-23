2474efbe3a529b5e6280eb9c200311a12da10111
Paul Delifer <paul.delifer@ikmail.com>
2023-02-23 15:42:44 +0100

    soap service

---
+++ src/app/components/calcul/calcul.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { SoapCalculService } from 'src/app/services/soap-calcul.service';
+
+@Component({
+  selector: 'app-calcul',
+  templateUrl: './calcul.component.html',
+  styleUrls: ['./calcul.component.scss']
+})
+export class CalculComponent implements OnInit {
+
+  distance!: number;
+  vitesse!: number;
+  autonomie!: number;
+  recharge!: number;
+
+  resultat!: number;
+
+  constructor(private soapCalcul: SoapCalculService) { }
+  ngOnInit(): void {
+    this.soapCalcul.calculDuration(60,45.7603831,4.849664,43.3,5.4,400.0,35.5).pipe(
+      map(value => this.resultat = value)
+    ).subscribe();
+  }
+
+
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
+
+    const dist = form.value.distance;
+    const vitt = form.value.vitesse;
+    const auto = form.value.autonomie;
+    const rech = form.value.recharge;
+
+
+  }
+
+}
---
+++ src/app/services/soap-calcul.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SoapCalculService {
+
+  private uri = 'http://localhost:8000';
+  private options = { responseType: 'text' as 'json' };
+
+  constructor(private http: HttpClient) { }
+
+  calculDuration(vitesse_moyene: number, lat1: number, lon1: number, lat2: number,lon2:number,autonomie:number, temps_recharge_min:number): Observable<any> {
+    const body = `
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:spy="info.802.calcul.soap">\
+      <soapenv:Header/>
+      <soapenv:Body>
+        <spy:calculer_temps_trajet>
+            <spy:vitesse_moyenne>${vitesse_moyene}</spy:vitesse_moyenne>
+            <spy:lat1>${lat1}</spy:lat1>
+            <spy:lon1>${lon1}</spy:lon1>
+            <spy:lat2>${lat2}</spy:lat2>
+            <spy:lon2>${lon2}</spy:lon2>
+            <spy:autonomie>${autonomie}</spy:autonomie>
+            <spy:temps_recharge_min>${temps_recharge_min}</spy:temps_recharge_min>
+        </spy:calculer_temps_trajet>
+      </soapenv:Body>
+    </soapenv:Envelope>`;
+    return this.http.post<any>(this.uri, body, this.options).pipe(
+      map(value => {
+        const data = value.split("calculer_temps_trajetResult");
+        let res = data[1];
+        res = res.replace(">","");
+        res = res.replace("</tns:","");
+        return res;
+      })
+    );
+  }
+}
